7c4d20dcb8b778375708a00f93bcca9af85981bb
Aleksandr <aleksandr@Aleksandrs-MacBook-Pro.local>
2021-10-12 00:05:14 +0300

    complete video-chat app

---
+++ index.mjs
@@ -0,0 +1,41 @@
+import express from "express";
+import {createServer} from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin: "*",
+        methods: ["GET","POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Sever is running.");
+});
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnd");
+    });
+
+    socket.on("callUser", ({userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit("callUser", {signal: signalData, from, name});
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Sever is listening on ${PORT}`);
+});
